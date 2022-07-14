bafee6c35fb37e09c9e64c0c59f86724d4bc4868
Pop <95447025+PopPersonalAccount@users.noreply.github.com>
2022-07-14 03:07:15 +0200

    Added Content

--- remoteinfo.lua
+++ remoteinfo.lua
@@ -1,1 +1,7 @@
-
+url = "https://poppersonalaccount-computercraftapi-5g5jq6446fpwv-5000.githubpreview.dev/"
+function post(content)
+  local post, err = http.post(url, content)
+  if not post then
+      printError(err)
+  end
+end
